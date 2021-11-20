376eb40a2e670951b9af004145970e92ad8a8edd
SirKevinC <16568166+sirkevinc@users.noreply.github.com>
2021-11-20 17:38:01 -0500

    port

--- api/index.js
+++ api/index.js
@@ -42,7 +42,7 @@ const server = new ApolloServer({
 
 server.start().then(res => {
     server.applyMiddleware({ app });
-    app.listen({ port: 4000 }, () => {
+    app.listen({ port: process.env.PORT || 4000 }, () => {
         console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
     });
 });
