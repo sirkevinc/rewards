4f2290bfc93843aef2534d12ab207004bdd16583
SirKevinC <16568166+sirkevinc@users.noreply.github.com>
2022-12-15 12:23:05 -0500

    idk

--- api/config/config.js
+++ api/config/config.js
@@ -5,14 +5,8 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres",
+    "port": 5432
   },
   "production": {
     "username": process.env.DB_USERNAME,
