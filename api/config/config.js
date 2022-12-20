8d7c0796920f1a7a868e980708f009ae5e6ee770
SirKevinC <16568166+sirkevinc@users.noreply.github.com>
2022-12-20 13:45:28 -0500

    Test

--- api/config/config.js
+++ api/config/config.js
@@ -3,9 +3,13 @@ module.exports = {
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB,
+    "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
+    "use_env_variable": 'DATABASE_URL',
+    "dialectOptions": {
+      "ssl": true
+    }
   },
   "production": {
     "username": process.env.DB_USERNAME,
